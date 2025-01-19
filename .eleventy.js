b8f81038eb9ab0062b800064f8285d3c6caac62a
Angelica <angelica.knudsen05@gmail.com>
2025-01-19 16:32:21 -0500

    hopefully images and css are preserved

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/posts/");
-  eleventyConfig.addPassthroughCopy("src/Images/");
+  eleventyConfig.addPassthroughCopy("src/images/");
   /*eleventyConfig.addPassthroughCopy({ "/src/favicons": "/" });*/
-  eleventyConfig.addPassthroughCopy("/bundle.css");
+  eleventyConfig.addPassthroughCopy("src/bundle.css");
   eleventyConfig.addPassthroughCopy("src/filters.js")
 
   return {
