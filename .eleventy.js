309a5846372485b12af2aebc047dfe86f611c422
Angelica <angelica.knudsen05@gmail.com>
2025-01-19 14:49:59 -0500

    got blog posts working

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/Posts/");
+  eleventyConfig.addPassthroughCopy("src/posts/");
   eleventyConfig.addPassthroughCopy("src/Images/");
   /*eleventyConfig.addPassthroughCopy({ "/src/favicons": "/" });*/
   eleventyConfig.addPassthroughCopy("/bundle.css");
+  eleventyConfig.addPassthroughCopy("src/filters.js")
 
   return {
     dir: {
